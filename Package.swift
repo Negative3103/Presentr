87c5810d8385779cb20682d7dd4a2a3375c43570
Khasan Davronbekov <76944086+Negative3103@users.noreply.github.com>
2025-04-23 17:24:18 +0500

    Create Package.swift

---
+++ Package.swift
@@ -0,0 +1,19 @@
+// swift-tools-version:5.5
+
+import PackageDescription
+
+let package = Package(
+    name: "Presentr",
+    platforms: [
+        .iOS(.v11)
+    ],
+    products: [
+        .library(name: "Presentr", targets: ["Presentr"])
+    ],
+    targets: [
+        .target(
+            name: "Presentr",
+            path: "Sources/Presentr"
+        )
+    ]
+)
